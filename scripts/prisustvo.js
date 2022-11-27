f18104c3f2b58d2a0f6fb1ee12f1c06bebdf73b4
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-11-27 13:42:43 +0100

    Obrisani import i export

--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -1,54 +1,46 @@
-import {TabelaPrisustvo} from '../scripts/TabelaPrisustvo.js'
-
 let div = document.getElementById("mainContent");
 
 let prisustvo = TabelaPrisustvo(div, {
-    "studenti": [{
-            "ime": "Neko Nekic",
-            "index": 12345
-        },
-        {
-            "ime": "Neko Drugic",
-            "index": 12346
-        },
-        {
-            "ime": "Suljo Suljic",
-            "index": 12312
-        }
-    ],
-    "prisustva": [{
-            "sedmica": 1,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12345
-        },
-        {
-            "sedmica": 1,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12346
-        },
-        {
-            "sedmica": 1,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12312
-        },
-        {
-            "sedmica": 2,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12312
-        },
-        {
-            "sedmica": 2,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12346
-        }
-    ],
-    "predmet": "Razvoj mobilnih aplikacija",
+	"studenti": [{
+			"ime": "Neko Nekic",
+			"index": 12345
+		},
+		{
+			"ime": "Drugi Neko",
+			"index": 12346
+		}
+	],
+	"prisustva": [{
+			"sedmica": 1,
+			"predavanja": 2,
+			"vjezbe": 1,
+			"index": 12345
+		},
+		{
+			"sedmica": 1,
+			"predavanja": 2,
+			"vjezbe": 2,
+			"index": 12346
+		},
+		{
+			"sedmica": 2,
+			"predavanja": 2,
+			"vjezbe": 0,
+			"index": 12345
+		},
+		{
+			"sedmica": 2,
+			"predavanja": 2,
+			"vjezbe": 0,
+			"index": 12346
+		}
+	],
+	"predmet": "Razvoj mobilnih aplikacija",
 	"brojPredavanjaSedmicno": 2,
 	"brojVjezbiSedmicno": 2
 }
-)
+);
+/*
+prisustvo.sljedecaSedmica();
+prisustvo.prethodnaSedmica();
+*/
